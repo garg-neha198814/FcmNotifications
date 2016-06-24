dba9e3fcae4ef176b7d6317f50388e990fa6e6ac
garg-neha198814 <nehamalout@gmail.com>
2016-06-24 10:08:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anil/fcmnotifications/AdsActivity.java
@@ -0,0 +1,57 @@
+package com.example.anil.fcmnotifications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
+public class AdsActivity extends AppCompatActivity {
+    private InterstitialAd interstitial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ads);
+
+        // Prepare the Interstitial Ad
+        interstitial = new InterstitialAd(AdsActivity.this);
+        // Insert the Ad Unit ID
+        interstitial.setAdUnitId("ca-app-pub-123456789/123456789");
+
+        //Locate the Banner Ad in activity_main.xml
+        AdView adView = (AdView) this.findViewById(R.id.adView);
+
+        // Request for Ads
+        AdRequest adRequest = new AdRequest.Builder()
+
+                // Add a test device to show Test Ads
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("")
+                .build();
+
+        // Load ads into Banner Ads
+        adView.loadAd(adRequest);
+
+        // Load ads into Interstitial Ads
+        interstitial.loadAd(adRequest);
+
+        // Prepare an Interstitial Ad Listener
+        interstitial.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                // Call displayInterstitial() function
+                displayInterstitial();
+            }
+        });
+    }
+    public void displayInterstitial() {
+        // If Ads are loaded, show Interstitial else show nothing.
+        if (interstitial.isLoaded()) {
+            interstitial.show();
+        }
+    }
+
+
+}
